7f6611a4a01b5b52d7b74b5f2c7d887db9e6a151
Stackbit Code Editor <projects@stackbit.com>
2021-07-24 00:57:35 +0000

    hero.js: changed

--- src/sections/hero.js
+++ src/sections/hero.js
@@ -34,7 +34,7 @@ const Hero = ({ contentModuleId }) => {
         <section className="hero container section mx-auto">
             <div className="hero__tagline">
                 <div className="hero__tagline-content-wrap">
-                    <h2 className="hero__tagline-title" data-sal="fade">{ content.node.heading }</h2>
+                    
                     <p className="hero__tagline-subtitle" data-sal="fade" data-sal-delay="100">{ content.node.subheading }</p>
                     <p className="hero__tagline-text" data-sal="fade" data-sal-delay="200">{ content.node.description.description }</p>
                     <a href={ content.node.ctaUrl }><button className="btn btn--primary mt-8" data-sal="fade" data-sal-delay="300">{ content.node.ctaText }</button></a>
